75e40f58e4e35317d4e888ff0962e6002fd8b369
Jamie Dumont <jamiedumont@icloud.com>
2018-03-31 13:20:43 +0100

    Per-template headers

--- site/themes/millican/src/js/millican.js
+++
@@ -1,8 +0,0 @@
-var menuButton = document.getElementById("menu-button");
-var siteNavClasses = document.getElementById("site-navigation").classList;
-
-function toggleMenu() {
-  return siteNavClasses.toggle("open");
-}
-
-menuButton.onclick = toggleMenu;
